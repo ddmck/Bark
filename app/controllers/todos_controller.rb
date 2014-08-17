4097c2ee430b82e0a3d7d46adaefee5112ea765b
Donald McKendrick <ddmckendrick@gmail.com>
2014-08-17 22:14:36 +0100

    Set user_id as current users id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -25,7 +25,7 @@ class TodosController < ApplicationController
   # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
-
+    @todo[:user_id] = current_user.id
     respond_to do |format|
       if @todo.save
         format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
